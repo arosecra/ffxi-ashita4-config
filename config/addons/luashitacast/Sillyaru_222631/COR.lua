2556d329770485774a2fd624ce0d915799acd3c8
arosecra <34662069+arosecra@users.noreply.github.com>
2021-11-22 20:05:50 -0600

    updated for ashita update

---
+++ config/addons/luashitacast/Sillyaru_222631/COR.lua
@@ -0,0 +1,41 @@
+local profile = {};
+local sets = {
+};
+
+profile.Sets = sets;
+
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+end
+
+profile.HandleDefault = function()
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+end
+
+return profile;
